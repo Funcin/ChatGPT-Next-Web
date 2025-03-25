0ed2b90d7070904c759b448095bd1c88d3bd22a7
Funcin <funcin@yahoo.com>
2025-03-25 23:47:54 +0800

    初始化仓库，添加 Google Analytics 和 AdSense 代码

---
+++ app/layout.tsx
@@ -0,0 +1,58 @@
+/* eslint-disable @next/next/no-page-custom-font */
+import "./styles/globals.scss";
+import "./styles/markdown.scss";
+import "./styles/highlight.scss";
+import { getClientConfig } from "./config/client";
+
+export const metadata = {
+  title: "ChatGPT Next Web",
+  description: "Your personal ChatGPT Chat Bot.",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#fafafa" },
+    { media: "(prefers-color-scheme: dark)", color: "#151515" },
+  ],
+  appleWebApp: {
+    title: "ChatGPT Next Web",
+    statusBarStyle: "default",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta name="config" content={JSON.stringify(getClientConfig())} />
+        <link rel="manifest" href="/site.webmanifest"></link>
+        <script src="/serviceWorkerRegister.js" defer></script>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TSZY7441F7"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-TSZY7441F7');
+            `,
+          }}
+        />
+        {/* Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1637076228079589"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
